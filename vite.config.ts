ac89642be52f1d3026e06674164a1b29cabd5b0e
tomvalois82 <121457446+tomvalois82@users.noreply.github.com>
2026-05-14 16:08:51 -0300

    Add allowedHosts configuration for server

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    
+    allowedHosts: [
+      "quepasa-vx-cloud.ppmwkh.easypanel.host",
+    ],
+    
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
